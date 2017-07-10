2ac37743eaebf890b8a805cf11fb736b9773767d
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-07-10 21:11:09 +0530

    Create user model database migrations

---
+++ db/migrations/20170710153703-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        type: Sequelize.STRING,
+        required: true
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        required: true
+      },
+      email: {
+          type: Sequelize.STRING,
+          required: true
+      },
+      password: {
+          type: Sequelize.STRING,
+          required: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
